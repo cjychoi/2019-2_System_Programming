fd886e9863a3cc5831dbf7d4985b48164b00fc4f
Andrew JunYoung Choi <54846646+cjychoi@users.noreply.github.com>
2019-11-11 15:54:21 +0900

    waitdemo 1 and 2

---
+++ 191111/waitdemo1.c
@@ -0,0 +1,35 @@
+/*	waitdemo1.c - shows how parent pauses until child finishes
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#define DELAY 2
+
+void main()
+{
+	int newpid;
+	void child_code(int), parent_code(int);
+	printf("before: mypid is %d\n", getpid());
+
+	if ((newpid = fork()) == -1)
+		perror("fork");
+	else if (newpid == 0)
+		child_code(DELAY);
+	else
+		parent_code(newpid);
+}
+
+/* new process takes a nap and then exits */
+void child_code(int delay)
+{
+	printf("child %d here. will sleep for %d seconds\n", getpid(), delay);
+	printf("child done. about to exit\n");
+	exit(17);
+}
+
+/* parent waits for child then prints a message */
+void parent_code(int childpid)
+{
+	int wait_rv;
+	wait_rv = wait(NULL); 
+	printf("done waiting for %d. wait returned: %d\n", childpid, wait_rv);
+}
